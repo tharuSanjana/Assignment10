0aa1e62a80c62a204d63c41d2a6ebe37c9530e07
tharuSanjana <tharushisanjana28@gmail.com>
2024-05-14 00:46:40 +0530

    inite

---
+++ js.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $('#btn7Num').on('click', function() {
+        $('#viewValue').append('7');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn8Num').on('click', function() {
+        $('#viewValue').append('8');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn9Num').on('click', function() {
+        $('#viewValue').append('9');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn4Num').on('click', function() {
+        $('#viewValue').append('4');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn5Num').on('click', function() {
+        $('#viewValue').append('5');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn6Num').on('click', function() {
+        $('#viewValue').append('6');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn1Num').on('click', function() {
+        $('#viewValue').append('1');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn2Num').on('click', function() {
+        $('#viewValue').append('2');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn3Num').on('click', function() {
+        $('#viewValue').append('3');
+    });
+});
+
+$(document).ready(function() {
+    $('#btn0Num').on('click', function() {
+        $('#viewValue').append('0');
+    });
+});
+$('#clear').on('click', function() {
+    $('#viewValue').text('');
+});
+
